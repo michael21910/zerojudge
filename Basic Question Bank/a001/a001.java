0e351ed3f30d6bc6e6f25893d7a5c2ee3259fb9a
michael21910 <faefae31910@gmail.com>
2021-09-16 00:13:02 +0800

    add a001 solution, java and python

---
+++ Basic Question Bank/a001/a001.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class a001 {
+    public static void main(String[] args) {
+        System.out.println("hello, " + new java.util.Scanner(System.in).nextLine());
+    }
+}
