40aa72dcfd9fb5e03b11a88f0da5e23da8ad7a83
michael21910 <faefae31910@gmail.com>
2021-09-16 01:04:48 +0800

    add a002 solution, python

--- Basic Question Bank/a001/a001.java
+++ Basic Question Bank/a001/a001.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class a001 {
     public static void main(String[] args) {
         System.out.println("hello, " + new java.util.Scanner(System.in).nextLine());
---
+++ Basic Question Bank/a002/a002.py
@@ -0,0 +1,2 @@
+num_list = str(input()).split(' ')
+print(int(num_list[0]) + int(num_list[1]))
